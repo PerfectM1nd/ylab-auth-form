e2e635bd35b6707bdd46a6560bf388b449a63efe
PerfectM1nd <gzagayko@gmail.com>
2023-11-14 20:31:43 +0300

    feat: make base url in vite.config different for PROD and DEV modes

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  css: {
-    postcss: "./postcss.config.js",
-  },
-  base: "ylab-auth-form",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react(), tsconfigPaths()],
+    css: {
+      postcss: "./postcss.config.js",
+    },
+    base: env.PROD ? "/ylab-auth-form" : "/",
+  };
 });
